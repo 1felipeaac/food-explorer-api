a787adeb78e8cdb2511aa2981c83df4002c5d615
Felipe Coelho <felipeaacoelho@gmail.com>
2023-11-16 13:40:24 -0300

    dotenv

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,18 @@
 const path = require("path")
+require('dotenv').config({ path: "../.env" });
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: process.env.CLIENT,
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
     pool:{
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
     },
-    useNullAsDefault: true,
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+      directory: path.resolve(__dirname,"src", "database", "knex", "migrations")
     },
-  }
+    useNullAsDefault: true
+  },
 };
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config')
 require("express-async-errors")
 
 const migrationsRun = require("./database/sqlite/migrations")
@@ -41,5 +42,5 @@ app.use((error, request, response, next) =>{
 })
 
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3333;
 app.listen(PORT, ()=> console.log(`Server is run in port: ${PORT}`));
